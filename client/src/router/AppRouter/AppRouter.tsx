c0043d0c32ff6a7935564ec519fa42dc8b75ccc2
Adrian <alextravc.342@gmail.com>
2023-12-27 11:52:15 -0500

    [ADD]: client | AppRouter configured with AuthLayout + LoginPage + RegisterPage skeletons

--- client/src/router/AppRouter/AppRouter.tsx
+++ client/src/router/AppRouter/AppRouter.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { AuthLayout } from '@/auth/layouts/AuthLayout';
 import { ShopLayout } from '@/ecommerce/layouts/ShopLayout';
+
+import { LoginPage } from '@/auth/pages/LoginPage';
+import { RegisterPage } from '@/auth/pages/RegisterPage';
 import { HomePage } from '@/ecommerce/pages/HomePage';
 
 export interface AppRouterProps {}
@@ -11,6 +15,12 @@ const AppRouter: React.FC<AppRouterProps> = () => {
       <Routes>
         <Route path="/" element={<ShopLayout />}>
           <Route index element={<HomePage />} />
+        </Route>
+
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="" element={<Navigate to="/auth/login" replace />} />
         </Route>
 
         <Route path="*" element={<Navigate to="/" replace />} />
