addc4b288cbae7204aaa99b96cba71ccbb1443ac
Adrian <alextravc.342@gmail.com>
2023-12-27 09:47:55 -0500

    [ADD]: register view method & LoginView class

--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password # hashear password
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import User
+from .serializers import RegisterUserSerializer, MyTokenObtainPairSerializer
+
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+    user = User.objects.create(
+        email=data['email'],
+        name=data['name'],
+        last_name=data['last_name'],
+        password=make_password(data['password']),
+    )
+    serializer = RegisterUserSerializer(user, many = False)
+    return Response(serializer.data)
+
+
+
+
+
+
+# ## esta view permite hacer login: returns access & refresh token
+class LoginView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
