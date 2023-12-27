a6468fe5c113243af9df6f02aa6841032ad8b734
Adrian <alextravc.342@gmail.com>
2023-12-27 11:38:45 -0500

    [ADD]: client | ShopLayout

---
+++ client/src/ecommerce/layouts/ShopLayout/ShopLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+
+import { Header } from '@/shared/components/ui';
+
+export interface ShopLayoutProps {}
+
+const ShopLayout: React.FC<ShopLayoutProps> = () => {
+  return (
+    <>
+      <Header />
+
+      <main className="min-h-[1000px] bg-white dark:bg-gray-900">
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default ShopLayout;
