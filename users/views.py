29116bb98fed20a4a714b34ef493373b289a9c25
Adrian <alextravc.342@gmail.com>
2023-12-27 18:06:07 -0500

    [ADD]: handlegin signup errors

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.hashers import make_password # hashear password
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.db import IntegrityError
+
 
 from .models import User
 from .serializers import RegisterUserSerializer, MyTokenObtainPairSerializer
@@ -9,15 +11,18 @@ from .serializers import RegisterUserSerializer, MyTokenObtainPairSerializer
 
 @api_view(['POST'])
 def register(request):
-    data = request.data
-    user = User.objects.create(
-        email=data['email'],
-        name=data['name'],
-        last_name=data['last_name'],
-        password=make_password(data['password']),
-    )
-    serializer = RegisterUserSerializer(user, many = False)
-    return Response(serializer.data)
+    try:
+        data = request.data
+        user = User.objects.create(
+            email=data['email'],
+            name=data['name'],
+            last_name=data['last_name'],
+            password=make_password(data['password']),
+        )
+        serializer = RegisterUserSerializer(user, many = False)
+        return Response(serializer.data)
+    except IntegrityError:
+        return Response({'error': 'Email already registered'}, status=400)
 
 
 
