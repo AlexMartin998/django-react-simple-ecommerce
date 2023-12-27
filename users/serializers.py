323634a43d23a90305713143d4e8f5460ea9f2b5
Adrian <alextravc.342@gmail.com>
2023-12-27 09:47:28 -0500

    [ADD]: RegisterUserSerializer & RegisterUserSerializer

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+# ### Serializers based on Existing Models
+from .models import User
+
+
+
+# ### Serializer: Python to JSON  &  Deserialize: JSON to Python
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # fields tuple q podran ser consultados (se enviaran en la response del view)
+        fields = ('id', 'email', 'name', 'last_name') # specifying fields
+        # fields = '__all__' # all fields
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email # set email in JWT
+        token['avatar'] = user.avatar.url
+        token['is_staff'] = user.is_staff # isAdmin?
+        return token
+
+
