3b356a3481586c5f29f340f481091e5af7aa6300
Adrian <alextravc.342@gmail.com>
2023-12-28 16:52:49 -0500

    [ADD]: client | darkMode: 'class' in tailwind css cnofig

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // dark class enable DarkMode
+
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
