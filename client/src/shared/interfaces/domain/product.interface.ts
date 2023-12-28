9a0e07099492fdd19449a08329e37ccaeff7b536
Adrian <alextravc.342@gmail.com>
2023-12-28 14:13:47 -0500

    [ADD]: client | Product interface

---
+++ client/src/shared/interfaces/domain/product.interface.ts
@@ -0,0 +1,17 @@
+import { Review } from '.';
+
+export interface Product {
+  id?: number;
+  reviews: Review[];
+  name: string;
+  image: File | string | null;
+  category: string;
+  count_in_stock: number;
+  created_at: string;
+  description?: string;
+  rating?: string;
+  num_reviews?: number;
+  price: string;
+  cuantity?: number;
+  user: number;
+}
