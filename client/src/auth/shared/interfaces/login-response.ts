b5bf05b5f60d94657eb65e1b87fd667d0593956e
Adrian <alextravc.342@gmail.com>
2023-12-29 12:13:35 -0500

    [ADD]: client | Token interface

--- client/src/auth/shared/interfaces/login-response.ts
+++ client/src/auth/shared/interfaces/login-response.ts
@@ -2,3 +2,13 @@ export interface LoginResponse {
   refresh: string;
   access: string;
 }
+
+export interface Token {
+  user_id: number;
+  exp: number;
+  is_staff: boolean;
+  email: string;
+  name: string;
+  last_name: string;
+  avatar: File | null;
+}
