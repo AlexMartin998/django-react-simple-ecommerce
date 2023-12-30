2c49314a53f568d5a8edcc259543dcf115636687
Adrian <alextravc.342@gmail.com>
2023-12-29 19:01:08 -0500

    [ADD]: client | ProductsTBody

---
+++ client/src/admin/pages/AdminPage/components/Products/ProductsTBody.tsx
@@ -0,0 +1,32 @@
+import { InfiniteData } from '@tanstack/react-query';
+
+import { ProductsResponse } from '@/shared/interfaces';
+import { ProductsTr } from '.';
+
+export type FetchedBodyProps = {
+  data: InfiniteData<ProductsResponse> | undefined;
+  results: any;
+};
+
+const ProductsTBody: React.FC<FetchedBodyProps> = ({ data, results }) => {
+  return (
+    <>
+      {results?.products?.length ? (
+        <tbody>
+          <ProductsTr data={results.products} />
+        </tbody>
+      ) : (
+        <>
+          {/* throgh each page of infiniteQuery: duplicate tbody */}
+          {data?.pages.map((page: any) => (
+            <tbody key={page.meta.next}>
+              <ProductsTr data={page.data} />
+            </tbody>
+          ))}
+        </>
+      )}
+    </>
+  );
+};
+
+export default ProductsTBody;
