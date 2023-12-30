8e736675745465e8f7bc0bca889043e04c3cb91b
Adrian <alextravc.342@gmail.com>
2023-12-29 19:02:51 -0500

    [ADD]: client | useProductDeleteMutation in products.slice

--- client/src/store/products/products.slice.ts
+++ client/src/store/products/products.slice.ts
@@ -1,8 +1,34 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
 
-import { ecomApi } from '@/shared/axios';
+import { ecomApi, ecomApiAuth } from '@/shared/axios';
 import { ProductsResponse } from '@/shared/interfaces';
 
+export const useInfiniteQueryProducts = () =>
+  useInfiniteQuery(['products'], getProducts, {
+    getNextPageParam: (page: any) => page.meta.next,
+  });
+
+export const useProductDeleteMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+      toast.success('Product deleted!');
+    },
+    onError: () => {
+      toast.error('Error!');
+    },
+  });
+};
+
+////* actions
 export const getProducts = async ({ pageParam = 1 }) => {
   const response = await ecomApi.get<ProductsResponse>(
     `/products/?page=${pageParam}&pages=9`
@@ -11,7 +37,5 @@ export const getProducts = async ({ pageParam = 1 }) => {
   return response.data;
 };
 
-export const useInfiniteQueryProducts = () =>
-  useInfiniteQuery(['products'], getProducts, {
-    getNextPageParam: (page: any) => page.meta.next,
-  });
+const deleteProduct = async (id: number) =>
+  ecomApiAuth.delete(`/products/delete/${id}/`);
