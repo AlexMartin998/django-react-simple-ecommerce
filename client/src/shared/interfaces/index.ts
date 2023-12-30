beeab2dc9bdf3e7484b4246d2fbaffa4d2f0b717
Adrian <alextravc.342@gmail.com>
2023-12-29 21:23:49 -0500

    [ADD]: client | ProductsSearchResponse

--- client/src/shared/interfaces/index.ts
+++ client/src/shared/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './domain';
 export * from './product-cart';
 export * from './products-response';
+export * from './products-search-response';
---
+++ client/src/shared/interfaces/products-search-response.ts
@@ -0,0 +1,5 @@
+import { Product } from '.';
+
+export interface ProductsSearchResponse {
+  products: Product[];
+}
