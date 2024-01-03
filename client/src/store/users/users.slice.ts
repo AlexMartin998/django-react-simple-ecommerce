c810d9ca271d4643a0f1e87ff608ede71d35b011
Adrian <alextravc.342@gmail.com>
2024-01-03 12:22:44 -0500

    [ADD]: client | useUserDeleteMutation

--- client/src/store/users/users.slice.ts
+++ client/src/store/users/users.slice.ts
@@ -1,5 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+
 import { ecomApiAuth } from '@/shared/axios';
 import { User } from '@/shared/interfaces';
+
+export const useUserDeleteMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (id: number) => {
+      await ecomApiAuth.delete(`/users/delete/${id}/`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] });
+      toast.success('User deleted!');
+    },
+    onError: () => {
+      toast.error('Error!');
+    },
+  });
+};
 
 export const getUsers = async () => {
   const response = await ecomApiAuth.get<User[]>('/users/');
