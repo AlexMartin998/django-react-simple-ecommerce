0c304a32cec82a64a22d3d04fbcfe84f2ef57020
Adrian <alextravc.342@gmail.com>
2024-01-03 18:21:07 -0500

    [ADD]: orders endpoints in urls.py

---
+++ orders/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+
+    path('', views.get_orders),
+    path('my/orders/', views.get_my_orders),
+    path('get/<int:id>/', views.get_order),
+    path('create/', views.create_order),
+    path('deliver/<int:pk>/', views.delivered),
+
+    # search by URL Params: ?query=something
+    path('search/', views.search),
+
+]
