0abc528a718c7153c82964a056f1d4658a9da998
Adrian <alextravc.342@gmail.com>
2024-01-03 18:16:41 -0500

    [ADD]: get_orders view method

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime
 
-# Create your views here.
+from .models import Order, OrderItem, ShippingAddress
+from .serializers import OrderSerializer
+from products.models import Product
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser]) # isAdmin 2nd way
+def get_orders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
