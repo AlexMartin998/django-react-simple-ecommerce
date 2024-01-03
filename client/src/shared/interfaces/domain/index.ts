366128bfdbb88c8bc2897005ebcb04a8054f185c
Adrian <alextravc.342@gmail.com>
2024-01-03 09:44:20 -0500

    [ADD]: client | User domain interface

--- client/src/shared/interfaces/domain/index.ts
+++ client/src/shared/interfaces/domain/index.ts
@@ -1,2 +1,3 @@
 export * from './product.interface';
 export * from './review.interface';
+export * from './user.interface';
---
+++ client/src/shared/interfaces/domain/user.interface.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id?: number;
+  avatar: File | null;
+  email: string;
+  name: string;
+  last_name: string;
+}
