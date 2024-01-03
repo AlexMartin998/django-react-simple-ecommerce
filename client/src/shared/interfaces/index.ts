f0c2b032b45c83694350f420d635352e9e82a6cd
Adrian <alextravc.342@gmail.com>
2024-01-03 14:35:53 -0500

    [ADD]: client | SearchUsersResponse

--- client/src/shared/interfaces/index.ts
+++ client/src/shared/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './domain';
 export * from './product-cart';
 export * from './products-response';
 export * from './products-search-response';
+export * from './users-search-response';
