eb7e7593cd80e63ad56396e2e46e8a2abf1052cc
Adrian <alextravc.342@gmail.com>
2024-01-04 14:33:10 -0500

    [ADD]: edit_profile user views method

--- users/views.py
+++ users/views.py
@@ -66,6 +66,24 @@ def delete_user(request, id):
 
 
 
+@api_view(['PUT'])
+def edit_profile(request, id):
+    try:
+        user = User.objects.get(pk=id)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.user == user:
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
 
 
 
