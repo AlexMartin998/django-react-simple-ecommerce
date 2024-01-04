5b81cf6418ab7a6470ad123cd8375875596aef58
Adrian <alextravc.342@gmail.com>
2024-01-04 11:26:26 -0500

    [ADD]: client | VITE_PAYPAL_CLIENT_ID env

--- client/src/shared/utils/get-evns.ts
+++ client/src/shared/utils/get-evns.ts
@@ -1,4 +1,5 @@
 export const getEnvs = () => ({
   VITE_MODE: import.meta.env.VITE_MODE, // testing
   VITE_API_URL: import.meta.env.VITE_API_URL,
+  PAYPAL_CLIENT_ID: import.meta.env.VITE_PAYPAL_CLIENT_ID,
 });
