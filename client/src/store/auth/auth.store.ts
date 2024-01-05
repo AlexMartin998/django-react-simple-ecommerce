0f4f1cd239d3da836b761f7daf92a4cb18de4749
Adrian <alextravc.342@gmail.com>
2024-01-05 11:03:37 -0500

    [ADD]: client | setUser in auth store

--- client/src/store/auth/auth.store.ts
+++ client/src/store/auth/auth.store.ts
@@ -3,36 +3,42 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 import { Token } from '@/auth/shared/interfaces';
+import { User } from '@/shared/interfaces';
+import { getUser } from '.';
 
 type State = {
   access: string;
   refresh: string;
   isAuth: boolean;
   isAdmin: boolean;
+  user: User | null;
 
   decodedToken: Token | null;
 };
 
 type Actions = {
-  setToken: (access: string, refresh: string) => void;
+  setToken: (access: string, refresh: string) => Promise<void>;
   logout: () => void;
+  setUserLogin: (userId: number | null) => Promise<void>;
+  setUser: (user: any) => void;
 };
 
 // persist returns a fn(), so we need to invoke it ()()
 export const useAuthStore = create<State & Actions>()(
   // persist by default in LocalStorage
   persist(
-    set => ({
+    (set, get) => ({
       ////* state
       access: '',
       refresh: '',
       isAuth: false,
       isAdmin: false,
       decodedToken: null,
+      user: null,
 
       ////* actions
       // LoginLike
-      setToken(access: string, refresh: string) {
+      setToken: async (access: string, refresh: string) => {
         // x como me lo envia Django
         const tokenDecoded: Token = jwtDecode(access);
 
@@ -43,9 +49,28 @@ export const useAuthStore = create<State & Actions>()(
           isAdmin: (!!access && !!refresh && tokenDecoded.is_staff) || false,
           decodedToken: tokenDecoded,
         }));
+
+        await get().setUserLogin(tokenDecoded.user_id);
+      },
+      setUserLogin: async (userId: number | null) => {
+        if (!userId) return get().logout();
+
+        const user = await getUser(userId ?? 0);
+        if (!user?.id) return get().logout();
+
+        set({ user: user });
+      },
+      setUser(user) {
+        set({ user: user });
       },
       logout() {
-        set({ access: '', refresh: '', isAuth: false, isAdmin: false });
+        set({
+          access: '',
+          refresh: '',
+          isAuth: false,
+          isAdmin: false,
+          user: null,
+        });
       },
     }),
 
