27113bd16dbda3a59bf31126d7395bb0527f87cd
Adrian <alextravc.342@gmail.com>
2024-01-04 20:23:07 -0500

    [ADD]: client | ProfilePage completed

--- client/src/dashboard/pages/ProfilePage/ProfilePage.tsx
+++ client/src/dashboard/pages/ProfilePage/ProfilePage.tsx
@@ -1,7 +1,24 @@
+import { useAuthStore } from '@/store/auth';
+import { useUiStore } from '@/store/ui';
+import { ProfileCard, ProfileForm } from '.';
+
 export interface ProfilePageProps {}
 
 const ProfilePage: React.FC<ProfilePageProps> = () => {
-  return <div>ProfilePage</div>;
+  const decodedToken = useAuthStore(s => s.decodedToken);
+  const isModalOpen = useUiStore(s => s.isModalOpen);
+
+  return (
+    <div className="flex justify-center pt-[6rem]">
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        {!isModalOpen ? (
+          <ProfileCard decodedToken={decodedToken} />
+        ) : (
+          <ProfileForm userId={decodedToken!.user_id} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
