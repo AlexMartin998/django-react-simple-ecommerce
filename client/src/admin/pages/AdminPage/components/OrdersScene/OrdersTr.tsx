5a57862a267ee420dbb227fcb43a79c1ad3e490a
Adrian <alextravc.342@gmail.com>
2024-01-05 16:28:15 -0500

    [ADD]: client | onComplete order with toggle btn

--- client/src/admin/pages/AdminPage/components/OrdersScene/OrdersTr.tsx
+++ client/src/admin/pages/AdminPage/components/OrdersScene/OrdersTr.tsx
@@ -11,9 +11,11 @@ const OrdersTr: React.FC<OrdersTrProps> = ({ data }) => {
   const completeOrderMutation = useCompleteOrderMutation();
 
   ///* handlers
-  const onComplete = (id: number) => {
+  const onComplete = (order: OrderResponse) => {
+    if (order.delivered_at) return;
+
     if (window.confirm('Are you sure?') === true) {
-      completeOrderMutation.mutate(id);
+      completeOrderMutation.mutate(order.id);
     }
   };
 
@@ -24,21 +26,7 @@ const OrdersTr: React.FC<OrdersTrProps> = ({ data }) => {
           key={order.id}
           className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
         >
-          <td className="w-4 p-4">
-            <div className="flex items-center">
-              <input
-                id="checkbox-table-search-1"
-                type="checkbox"
-                checked={order.is_delivered}
-                onChange={() => onComplete(order.id)}
-                // onClick={() => onComplete(order.id)}
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label htmlFor="checkbox-table-search-1" className="sr-only">
-                checkbox
-              </label>
-            </div>
-          </td>
+          {/* ------ ID ------ */}
           <th
             scope="row"
             className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -46,24 +34,44 @@ const OrdersTr: React.FC<OrdersTrProps> = ({ data }) => {
             {order.id}
           </th>
 
-          <td className="px-6 py-4">{order.created_at.slice(0, 10)}</td>
-
-          <td className="px-6 py-4">
-            {order.delivered_at !== null && (
-              <>{order.delivered_at.slice(0, 10)}</>
-            )}
-          </td>
-
+          {/* ------ user email ------ */}
           <td className="px-6 py-4">{order.user}</td>
 
+          {/* ------ Total Amount ------ */}
           <td className="px-6 py-4">$ {order.total_price}</td>
 
-          <td className="px-6 py-4">
+          {/* ------ Products - View ------ */}
+          <td className="px-4 py-4 text-center hidden md:table-cell">
             <Link to={`/dashboard/orders/${order.id}`}>See</Link>
           </td>
 
-          <td className="px-6 py-4">
+          {/* ------ Address ------ */}
+          <td className="px-4 py-4 text-center hidden md:table-cell">
             <Link to={`/dashboard/orders/${order.id}`}>See</Link>
+          </td>
+
+          {/* ------ toggle delivery ------ */}
+          <td className="px-6 py-4 text-center">
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                checked={order?.delivered_at ? true : false}
+                disabled={order?.delivered_at ? true : false}
+                onChange={() => onComplete(order)}
+              />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
+          </td>
+
+          {/* ------ createdAt ------ */}
+          <td className="px-6 py-4 hidden md:table-cell">
+            {order.created_at.slice(0, 10)}
+          </td>
+
+          {/* ------ deliveredAt ------ */}
+          <td className="px-6 py-4 hidden md:table-cell">
+            {order?.delivered_at?.slice(0, 10)}
           </td>
         </tr>
       ))}
