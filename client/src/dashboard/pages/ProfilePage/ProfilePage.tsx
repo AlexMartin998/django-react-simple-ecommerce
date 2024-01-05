a7b6e596bef14f1fbe3d52f9a69be31bab055b4c
Adrian <alextravc.342@gmail.com>
2024-01-04 21:42:52 -0500

    [ADD]: client | MyOrders included in ProfilePage

--- client/src/dashboard/pages/ProfilePage/ProfilePage.tsx
+++ client/src/dashboard/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import { useAuthStore } from '@/store/auth';
+import { useGetMyOrders } from '@/store/cart';
 import { useUiStore } from '@/store/ui';
-import { ProfileCard, ProfileForm } from '.';
+import { MyOrders, ProfileCard, ProfileForm } from '.';
 
 export interface ProfilePageProps {}
 
@@ -8,8 +9,10 @@ const ProfilePage: React.FC<ProfilePageProps> = () => {
   const decodedToken = useAuthStore(s => s.decodedToken);
   const isModalOpen = useUiStore(s => s.isModalOpen);
 
+  const { data, isLoading: isLoadingOrders } = useGetMyOrders();
+
   return (
-    <div className="flex justify-center pt-[6rem]">
+    <div className="flex flex-col justify-center items-center pt-[6rem] gap-7">
       <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         {!isModalOpen ? (
           <ProfileCard decodedToken={decodedToken} />
@@ -17,6 +20,13 @@ const ProfilePage: React.FC<ProfilePageProps> = () => {
           <ProfileForm userId={decodedToken!.user_id} />
         )}
       </div>
+
+      {/* ========= My Orders ========= */}
+      <MyOrders
+        myOrders={data}
+        isModalOpen={isModalOpen}
+        isLoadingOrders={isLoadingOrders}
+      />
     </div>
   );
 };
