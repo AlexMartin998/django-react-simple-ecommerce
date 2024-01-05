0b87a84a17a02c7b3a89321de10f4faacdbefec1
Adrian <alextravc.342@gmail.com>
2024-01-05 16:28:41 -0500

    [ADD]: client | OrdersScene with a responsive table

--- client/src/admin/pages/AdminPage/components/OrdersScene/OrdersScene.tsx
+++ client/src/admin/pages/AdminPage/components/OrdersScene/OrdersScene.tsx
@@ -24,38 +24,30 @@ const OrdersScene: React.FC<OrderSceneProps> = ({ results }) => {
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="p-4">
-              <div className="flex items-center">
-                <input
-                  id="checkbox-all-search"
-                  type="checkbox"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label htmlFor="checkbox-all-search" className="sr-only">
-                  checkbox
-                </label>
-              </div>
-            </th>
             <th scope="col" className="px-6 py-3">
               Order id
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Created at
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Delivered at
             </th>
             <th scope="col" className="px-6 py-3">
               User
             </th>
             <th scope="col" className="px-6 py-3">
-              Total Price
+              Total <br />
+              Price
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-4 py-3 hidden md:table-cell">
               Products
             </th>
+            <th scope="col" className="px-4 py-3 hidden md:table-cell">
+              Shipping <br /> Address
+            </th>
             <th scope="col" className="px-6 py-3">
-              Shipping Address
+              Status
+            </th>
+            <th scope="col" className="px-6 py-3 hidden md:table-cell">
+              Created at
+            </th>
+            <th scope="col" className="px-6 py-3 hidden md:table-cell">
+              Delivered at
             </th>
           </tr>
         </thead>
