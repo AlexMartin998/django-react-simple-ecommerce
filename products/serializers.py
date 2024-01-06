33e58e2fcfc788a9dcf5ca54d0fdbc4ab5c18574
Adrian <alextravc.342@gmail.com>
2024-01-05 19:55:04 -0500

    [ADD]: ReviewSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -7,7 +7,8 @@ from .models import Product, Reviews
 
 # ### Serializer: Python to JSON  &  Deserialize: JSON to Python
 class ProductSerializer(serializers.ModelSerializer):
-    # traer las reviews d c/product
+    # ## agregar mas cosas al serializer basado en las Relaciones
+    # # obtener x methods especificos d esta class/model (get_avatar)
     reviews = serializers.SerializerMethodField(read_only = True)
 
     class Meta:
@@ -15,7 +16,8 @@ class ProductSerializer(serializers.ModelSerializer):
         # fields tuple q podran ser consultados (se enviaran en la response del view)
         fields = '__all__' # all fields
 
-    def get_reviews(self, obj):
+    # # Methods para el   SerializerMethodField
+    def get_reviews(self, obj): # traer las reviews d c/product
         reviews = obj.reviews_set.all()
         serializer = ReviewSerializer(reviews, many=True)
         return serializer.data
@@ -25,11 +27,12 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     # ## agregar mas cosas al serializer basado en las Relaciones
-    avatar = serializers.SerializerMethodField(
-        source = 'user.avatar.url'
-    )
     user = serializers.ReadOnlyField(
         source = 'user.email'
+    )
+    # # obtener x methods especificos d esta class/model (get_avatar)
+    avatar = serializers.SerializerMethodField(
+        source = 'user.avatar.url'
     )
 
     class Meta:
@@ -37,6 +40,7 @@ class ReviewSerializer(serializers.ModelSerializer):
         # fields tuple q podran ser consultados (se enviaran en la response del view)
         fields = '__all__' # all fields
 
+    # # Methods para el   SerializerMethodField
     def get_avatar(self, obj):
         return obj.user.avatar.url
 
